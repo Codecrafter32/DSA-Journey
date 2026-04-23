9bb92e3e5f9298ed74cf80d02ab0ee3474ffb443
Annasha Bhattacharjee <annashabhattacharya9@gmail.com>
2026-04-23 14:41:11 +0530

    Solving Recursion problems-basic

--- src/recursion/Reversenumb.java
+++ src/recursion/Reversenumb.java
@@ -1,13 +1,27 @@
+// // Print numbers 5 to 1
+// public class Reversenumb{
+//     public static void printnumbers(int n) {
+//         if(n == 0){
+//             return;
+//         }
+//         System.out.println(n);
+//         printnumbers(n-1);
+//     }
+//     public static void main(String[] args) {
+//         int n = 5;
+//         printnumbers(n);
+//     }
+// }
 public class Reversenumb{
     public static void printnumbers(int n) {
-        if(n == 0){
+        if(n >= 6){
             return;
         }
         System.out.println(n);
-        printnumbers(n-1);
+        printnumbers(n+1);
     }
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1;
         printnumbers(n);
     }
 }
